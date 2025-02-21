9d13d9a5077713d200f0fb5a21c2e2b9b09384f6
Brett Kelly <brett@brettkelly.org>
2025-02-21 15:34:47 -0800

    more nvim stuf

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -3,25 +3,32 @@ vim.opt_local.formatoptions:append("r") -- Auto-insert comment leader after <Ent
 vim.opt_local.formatoptions:append("o") -- Auto-insert comment leader after o/O
 vim.opt_local.formatoptions:append("n") -- Recognize numbered lists
 
+-- Soft wrapping at 80 characters for Markdown files
+vim.opt_local.wrap = true -- Enable soft wrapping
+vim.opt_local.linebreak = true -- Wrap at word boundaries
+vim.opt_local.textwidth = 80 -- Set text width to 80 (optional, for consistency)
+vim.opt_local.breakindent = true -- Indent wrapped lines (optional, for readability)
+vim.opt_local.formatoptions:remove("t") -- Prevent auto-hard-wrapping
+
 -- Checkbox behavior
-vim.keymap.set("i", "<CR>", function()
-    local line = vim.api.nvim_get_current_line()
-    local cursor = vim.api.nvim_win_get_cursor(0)
-    local col = cursor[2]
-    
-    -- Check specifically for checkbox pattern
-    local checkbox_pattern = "^%s*-%s*%[%s?%]%s*"
-    
-    if line:match(checkbox_pattern) then
-        -- If the line is empty except for the checkbox, remove it
-        if line:match(checkbox_pattern .. "$") then
-            vim.api.nvim_set_current_line("")
-            return "<CR>"
-        end
-        -- Otherwise, create a new checkbox
-        local indent = line:match("^%s*")
-        return "<CR>" .. indent .. "- [ ] "
-    end
-    
-    return "<CR>"
-end, { buffer = true, expr = true })
+-- vim.keymap.set("i", "<CR>", function()
+-- 	local line = vim.api.nvim_get_current_line()
+-- 	local cursor = vim.api.nvim_win_get_cursor(0)
+-- 	local col = cursor[2]
+--
+-- 	-- Check specifically for checkbox pattern
+-- 	local checkbox_pattern = "^%s*-%s*%[%s?%]%s*"
+--
+-- 	if line:match(checkbox_pattern) then
+-- 		-- If the line is empty except for the checkbox, remove it
+-- 		if line:match(checkbox_pattern .. "$") then
+-- 			vim.api.nvim_set_current_line("")
+-- 			return "<CR>"
+-- 		end
+-- 		-- Otherwise, create a new checkbox
+-- 		local indent = line:match("^%s*")
+-- 		return "<CR>" .. indent .. "- [ ] "
+-- 	end
+--
+-- 	return "<CR>"
+-- end, { buffer = true, expr = true })
