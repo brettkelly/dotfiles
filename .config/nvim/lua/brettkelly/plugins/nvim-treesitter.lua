82ccde6d8707a602a915c103d5ac1facf0105ccc
Brett Kelly <brett@brettkelly.org>
2025-08-29 12:00:45 -0700

    markview, more ssh hosts, yet another nvim lock file

--- .config/nvim/lua/brettkelly/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/brettkelly/plugins/nvim-treesitter.lua
@@ -1,18 +1,34 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = { "OXY2DEV/markview.nvim" },
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = { "c","python", "php", "sql", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html","markdown","markdown_inline" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-    end,
-    opts = {
-        indent = { enable = true },
-        highlight = { enable = true }
-    }
- }
+		configs.setup({
+			ensure_installed = {
+				"c",
+				"python",
+				"php",
+				"sql",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"elixir",
+				"heex",
+				"javascript",
+				"html",
+				"markdown",
+				"markdown_inline",
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
+	opts = {
+		indent = { enable = true },
+		highlight = { enable = true },
+	},
+}
